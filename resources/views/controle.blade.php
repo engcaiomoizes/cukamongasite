40d2d39f9d50025fce10a097d935a77a926a0e53
Caio Moizés Santos <engcaiomoizes@gmail.com>
2023-07-20 10:40:11 -0300

    Vercel configs

--- resources/views/controle.blade.php
+++ resources/views/controle.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cukamonga Site - Controle</title>
     @vite(['resources/js/app.js'])
-    <link rel="stylesheet" href="/css/style.css">
+    @vit(['resources/css/app.css'])
 </head>
 <body>
     <div class="container-login">
