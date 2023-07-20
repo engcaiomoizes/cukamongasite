d52882a70d46ab182c6b367c4ac95211eaf8806a
Caio Moizés Santos <engcaiomoizes@gmail.com>
2023-07-20 10:34:17 -0300

    Vercel configs

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
     <link rel="stylesheet" href="/css/style.css">
+    @vite(['resources/css/app.css'])
 </head>
 <body>
     <header>
